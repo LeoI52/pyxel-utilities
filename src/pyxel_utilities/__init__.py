2d13150939e15d7599a323ea7347bd26cc111dbc
Léo Imbert <leoimbert5210@gmail.com>
2025-06-04 21:27:08 +0200

    Added game and var

--- src/pyxel_utilities/__init__.py
+++ src/pyxel_utilities/__init__.py
@@ -3,3 +3,13 @@
 @created : 15/10/2024
 @updated : 04/06/2025
 """
+
+from .game import PyxelManager, Scene
+from .var import DEFAULT_PYXEL_COLORS
+
+__version__ = "0.1.1"
+
+__all__ = [
+    "PyxelManager", "Scene",
+    "DEFAULT_PYXEL_COLORS"
+]
---
+++ src/pyxel_utilities/game.py
@@ -0,0 +1,119 @@
+"""
+@author : Léo Imbert
+@created : 15/10/2024
+@updated : 04/06/2025
+"""
+
+from .var import DEFAULT_PYXEL_COLORS
+import random
+import pyxel
+
+class PyxelManager:
+
+    def __init__(self, width:int, height:int, scenes:list, default_scene_id:int=0, fps:int=60, fullscreen:bool=False, mouse:bool=False, quit_key:int=pyxel.KEY_ESCAPE, camera_x:int=0, camera_y:int=0):
+        
+        self.__fps = fps
+        self.__scenes = scenes
+
+        for scene in self.__scenes:
+            if scene.id == default_scene_id:
+                self.__current_scene = scene
+
+        self.__cam_x = self.__cam_tx = camera_x
+        self.__cam_y = self.__cam_ty = camera_y
+        self.__shake_amount = 0
+        self.__sub_shake_amount = 0
+
+        pyxel.init(width, height, fps=self.__fps, quit_key=quit_key)
+        pyxel.fullscreen(fullscreen)
+        pyxel.mouse(mouse)
+
+        if self.__current_scene.pyxres_path:
+            pyxel.load(self.__current_scene.pyxres_path)
+        pyxel.title(self.__current_scene.title)
+        pyxel.screen_mode(self.__current_scene.screen_mode)
+        pyxel.colors.from_list(self.__current_scene.palette)
+
+    @property
+    def camera_x(self)-> int:
+        return self.__cam_x
+    
+    @property
+    def camera_y(self)-> int:
+        self.__cam_y
+
+    @property
+    def mouse_x(self)-> int:
+        return self.__cam_x + pyxel.mouse_x
+    
+    @property
+    def mouse_y(self)-> int:
+        return self.__cam_y + pyxel.mouse_y
+    
+    @property
+    def fps(self)-> int:
+        return self.__fps
+    
+    def set_camera(self, new_camera_x:int, new_camera_y:int):
+        self.__cam_x = self.__cam_tx = new_camera_x
+        self.__cam_y = self.__cam_ty = new_camera_y
+
+    def move_camera(self, new_camera_x:int, new_camera_y:int):
+        self.__cam_tx = new_camera_x
+        self.__cam_ty = new_camera_y
+
+    def shake_camera(self, amout:int, sub_amount:float):
+        self.__shake_amount = amout
+        self.__sub_shake_amount = sub_amount
+
+    def change_scene(self, new_scene_id:int, new_camera_x:int=0, new_camera_y:int=0, action=None):
+        self.set_camera(new_camera_x, new_camera_y)
+
+        for scene in self.__scenes:
+            if scene.id == new_scene_id:
+                self.__current_scene = scene
+
+        if self.__current_scene.pyxres_path:
+            pyxel.load(self.__current_scene.pyxres_path)
+        pyxel.title(self.__current_scene.title)
+        pyxel.screen_mode(self.__current_scene.screen_mode)
+        pyxel.colors.from_list(self.__current_scene.palette)
+
+        if action:
+            action()
+
+    def apply_palette_effect(self, effect_function, **kwargs):
+        pyxel.colors.from_list(effect_function(self.__current_scene.palette, kwargs))
+
+    def reset_palette(self):
+        pyxel.colors.from_list(self.__current_scene.palette)
+
+    def update(self):
+        self.__cam_x += (self.__cam_tx - self.__cam_x) * 0.1
+        self.__cam_y += (self.__cam_ty - self.__cam_y) * 0.1
+
+        if self.__shake_amount > 0:
+            amount = int(self.__shake_amount)
+            pyxel.camera(self.__cam_x + random.randint(-amount, amount), self.__cam_y + random.randint(-amount, amount))
+            self.__shake_amount -= self.__sub_shake_amount
+        else:
+            pyxel.camera(self.__cam_x, self.__cam_y)
+
+        self.__current_scene.update()
+
+    def draw(self):
+        self.__current_scene.draw()
+
+    def run(self):
+        pyxel.run(self.update, self.draw)
+
+class Scene:
+
+    def __init__(self, id:int, title:str, update, draw, pyxres_path:str=None, palette:list=DEFAULT_PYXEL_COLORS, screen_mode:int=0):
+        self.id = id
+        self.title = title
+        self.update = update
+        self.draw = draw
+        self.pyxres_path = pyxres_path
+        self.palette = palette
+        self.screen_mode = screen_mode
